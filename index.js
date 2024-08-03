40c7fc03e90b00ce9feb95324b537bb3648bd3db
KLM <122469699+KLMLEVRAI@users.noreply.github.com>
2024-08-03 13:08:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,3 +63,11 @@ const loadEvents = (dir = "./events/") => {
 
 loadEvents();
 loadCommands();
+client.user.setPresence({
+            status: 'dnd',
+            activity: {
+                name: 'yanis',
+                type: 'STREAMING',
+                url: 'https://twitch.tv/inoxtag'
+            }
+        });
